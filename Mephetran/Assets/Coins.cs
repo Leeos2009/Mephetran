ea217df9de5ca4d6162b6af2637c809dd7a18dd5
LAPTOP-AN4LIN7I\Lee O'Sullivan <lee.osullivan@students.ittralee.ie>
2018-02-14 10:18:36 +0000

    Starting Maze Generation

--- Mephetran/Assets/Coins.cs
+++ Mephetran/Assets/Coins.cs
@@ -13,12 +13,5 @@ public class Coins : MonoBehaviour {
 	void Update () {
 		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
-		void OnTriggerEnter(Collider other) 
-	{
-		if (other.gameObject.CompareTag ("Coin"))
-		{
-			other.gameObject.SetActive (false);
-			score = score + 100;
-		}
-	}
+	
 }
--- Mephetran/Assets/MazeGenerator.cs
+++ Mephetran/Assets/MazeGenerator.cs
@@ -6,18 +6,32 @@ public class MazeGenerator : MonoBehaviour {
 
 	public int width = 5;
 	public int length = 5;
+	private int i, j;
 	// Use this for initialization
 	void Start () {
 		GameObject Wall = new GameObject();
+		//Wall.transform.parent(this.transform);
 		//mazeCreation();
-		for(int i = 0; i <= width; i++)
+		for(i = 0; i <= width; i++)
 		{
-			for(int j = 0; j <= length; j++)
+			for(j = 0; j < length; j++)
 			{
-				Vector3 wallPosition = new Vector3(1 * i, 1, 1 * j);
+				Vector3 wallPosition = new Vector3(1 * (i + i) - 2, 1, 1 * (j + j));
 				Wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				Wall.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+				Wall.transform.localScale += new Vector3(-0.5f, 0, 1f);
+				Wall.name = "AAA";
 				Instantiate(Wall, wallPosition, Quaternion.identity);
+			}
+		}
+		for(i = 0; i < width; i++)
+		{
+			for(j = 0; j <= length; j++)
+			{
+				Vector3 wallPosition = new Vector3(1 * (i + i) - 1, 1, 1 * (j + j) - 1);
+				Wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				Wall.transform.localScale += new Vector3(-0.5f, 0, 1f);
+				//Wall.transform.Rotate += new Vector3(0, 90, 0);
+				Instantiate(Wall, wallPosition, Quaternion.Euler(0.0f, 90.0f, 0.0f));
 			}
 		}
 	}
